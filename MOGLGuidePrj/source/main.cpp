26df6278a7c60989f909ee315afef40b0f5aa7f4
cesar.gomez <csrgmz@gmail.com>
2020-04-10 20:31:31 -0500

    *Adding Floor rectangle(embedded in the same cube geometry, and duplicating the cube for simulating reflection

--- MOGLGuidePrj/source/main.cpp
+++ MOGLGuidePrj/source/main.cpp
@@ -133,8 +133,8 @@ int main(int argc, char* argv[])
 
 	//////////////////////////////////////////////////////////////////////////////
 	//Transformation stuff
-	GLfloat speed = 0.0f;
-	GLfloat angle = -45.0f;
+	/*GLfloat speed = 0.0f;
+	GLfloat angle = -45.0f;*/
 
 	//////////////////////////////////////////////////////////////////////////////
 	glEnable(GL_DEPTH_TEST);
@@ -147,10 +147,10 @@ int main(int argc, char* argv[])
 		{
 			if (windowEvent.type == SDL_QUIT) break;			
 			if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
-			if (windowEvent.type == SDL_KEYDOWN && windowEvent.key.keysym.sym == SDLK_SPACE)
+			/*if (windowEvent.type == SDL_KEYDOWN && windowEvent.key.keysym.sym == SDLK_SPACE)
 			{
 				speed = 180.0f;
-			}			
+			}	*/		
 		}		
 
 		/////////////////////////////////		//Clear Buffers
@@ -169,15 +169,15 @@ int main(int argc, char* argv[])
 
 		/////////////////////////////////		//Transformation Uniforms
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, time * glm::radians(angle) * 0.1f, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::rotate(model, glm::radians(time) * 15, glm::vec3(0.0f, 0.0f, 1.0f));
 
-		GLfloat s = sin(time * 5.0f) * 0.25f + 0.75f;
+		GLfloat s = sin(time * 2.0f) * 0.25f + 1.5f;
 		model = glm::scale(model, glm::vec3(s, s, s)); //scaling the image with a sin function
 
 		glm::mat4 view = glm::lookAt(
-			glm::vec3(0.0f, 1.0f, 2.0f), //camera position
-			glm::vec3(0.0f, 0.0f, 0.0f), //point of interest
-			glm::vec3(0.0f, 1.0f, 0.0f)  //y is UP,    x,z is the "plane"  ground			
+			glm::vec3(0.0f, -5.0f, 2.0f), //camera position
+			glm::vec3(0.0f, 0.0f, -0.5f), //point of interest
+			glm::vec3(0.0f, 0.0f, 1.0f)  //y is UP,    x,z is the "plane"  ground			
 			);
 
 		glm::mat4 proj = glm::perspective(glm::radians(45.0f), 1024.0f / 768.0f, 1.0f, 10.0f);
@@ -186,8 +186,8 @@ int main(int argc, char* argv[])
 		glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
 
-		speed /= 1.0f + time;
-		angle += 5 * speed * time;
+		/*speed /= 1.0f + time;
+		angle += 5 * speed * time;*/
 
 		/////////////////////////////////		//Drawing the stuff
 		
@@ -195,6 +195,16 @@ int main(int argc, char* argv[])
 			glDrawArrays(GL_TRIANGLES, 0, 36);		//Drawing the vertices from vbo directly
 		else
 			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);	//6 elements or 6 vertexElements ~ 2 triangles
+
+		//Draw the floor
+		
+		glDrawArrays(GL_TRIANGLES, 36, 6);
+
+		//Transform and draw the second cube
+
+		model = glm::scale(glm::translate(model, glm::vec3(0, 0, -1)), glm::vec3(1, 1, -1));
+		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		
 		/////////////////////////////////
 
